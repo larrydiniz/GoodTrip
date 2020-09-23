f251c2402397a690854a93ecb12f0b0bf04399df
mouraCorazim <lucasmouracorazim1@gmail.com>
2020-09-23 01:01:14 -0300

    PI-LOGIN: toggling class on login modal

--- front-end/public/scripts/main.js
+++ front-end/public/scripts/main.js
@@ -1,15 +1,25 @@
 /************** Menu Overlay ************/
 const nav = document.getElementById("menu");
 const btn = document.querySelector(".menu-landing");
+const btnMenuLogin = document.querySelector("button#entrar.menu");
+const divLoginContent = document.querySelector("div.login.conteudo");
+const btnLoginClose = document.querySelector("button.login.fechar");
 
-function menu (){
-    let show = nav.classList.contains("show");
+function toggleClass(element, toggClass){
 
-        if(show == false){
-            nav.classList.add("show");
-        }else{
-            nav.classList.remove("show");
-        }
+	return function(){
+		const classList = element.classList
+
+	    if(classList.contains(toggClass)){
+	        classList.remove(toggClass);
+	    }
+	    else{
+	        classList.add(toggClass);
+	    }
+	}
 }
 
-btn.addEventListener("click", menu) 
+btn.addEventListener("click", toggleClass(nav, "show"));
+nav.addEventListener("click", toggleClass(nav, "show"));
+btnMenuLogin.addEventListener("click", toggleClass(divLoginContent, "show"));
+btnLoginClose.addEventListener("click", toggleClass(divLoginContent, "show"));
