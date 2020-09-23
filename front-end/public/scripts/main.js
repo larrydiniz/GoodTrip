c1c9ecae6af158a3fecd50d51ec6b788d68b2bd4
mouraCorazim <lucasmouracorazim1@gmail.com>
2020-09-23 03:20:57 -0300

    PI-CADASTRO: Mostrar modais e overlay via eventos

--- front-end/public/scripts/main.js
+++ front-end/public/scripts/main.js
@@ -2,11 +2,15 @@
 const nav = document.getElementById("menu");
 const btn = document.querySelector(".menu-landing");
 const btnMenuLogin = document.querySelector("button#entrar.menu");
+const btnMenuRegister = document.querySelector("button#cadastrar.menu");
 const divLoginContent = document.querySelector("div.login.conteudo");
+const divRegisterContent = document.querySelector("div.cadastro.conteudo");
 const btnLoginClose = document.querySelector("button.login.fechar");
+const btnRegisterClose = document.querySelector("button.cadastro.fechar");
 const overlay = document.querySelector("div.overlay");
 
 function toggleClass(element, toggClass){
+	const toggling = new Event('toggling');
 
 	return function(){
 		const classList = element.classList
@@ -17,12 +21,18 @@ function toggleClass(element, toggClass){
 	    else{
 	        classList.add(toggClass);
 	    }
+
+	    element.dispatchEvent(toggling);
 	}
 }
 
 btn.addEventListener("click", toggleClass(nav, "show"));
 nav.addEventListener("click", toggleClass(nav, "show"));
+
 btnMenuLogin.addEventListener("click", toggleClass(divLoginContent, "show"));
-btnMenuLogin.addEventListener("click", toggleClass(overlay, "show"));
 btnLoginClose.addEventListener("click", toggleClass(divLoginContent, "show"));
-btnLoginClose.addEventListener("click", toggleClass(overlay, "show"));
+divLoginContent.addEventListener("toggling", toggleClass(overlay, "show"));
+
+btnMenuRegister.addEventListener("click", toggleClass(divRegisterContent, "show"));
+btnRegisterClose.addEventListener("click", toggleClass(divRegisterContent, "show"));
+divRegisterContent.addEventListener("toggling", toggleClass(overlay, "show"));
