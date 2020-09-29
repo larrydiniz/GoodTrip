726be82aae80e2ab419ca2534044d51370c8270d
Lucas Moura Corazim <lucasmouracorazim1@gmail.com>
2020-09-29 18:37:48 -0300

    modais: melhorando descriminação

--- front-end/public/scripts/main.js
+++ front-end/public/scripts/main.js
@@ -10,34 +10,34 @@ const btnRegisterClose = document.querySelector("button.cadastro.fechar");
 const ulNav = document.querySelector("ul");
 const overlay = document.querySelector("div.overlay");
 
-function toggleClass(element, toggClass){
+function classToggler({element, toggleClass}){
 	const toggling = new Event('toggling');
 
 	return function(){
 		const classList = element.classList
 
-	    if(classList.contains(toggClass)){
-	        classList.remove(toggClass);
+	    if(classList.contains(toggleClass)){
+	        classList.remove(toggleClass);
 	    }
 	    else{
-	        classList.add(toggClass);
+	        classList.add(toggleClass);
 	    }
 
 	    element.dispatchEvent(toggling);
 	}
 }
 
-btn.addEventListener("click", toggleClass(nav, "show"));
-nav.addEventListener("click", toggleClass(nav, "show"));
-overlay.addEventListener("toggling", toggleClass(ulNav, "hide"));
+btn.addEventListener("click", classToggler({element: nav, toggleClass: "show"}));
+nav.addEventListener("click", classToggler({element: nav, toggleClass: "show"}));
+overlay.addEventListener("toggling", classToggler({element: ulNav, toggleClass: "hide"}));
 
-btnMenuLogin.addEventListener("click", toggleClass(divLoginContent, "show"));
-btnLoginClose.addEventListener("click", toggleClass(divLoginContent, "show"));
-divLoginContent.addEventListener("toggling", toggleClass(overlay, "show"));
+btnMenuLogin.addEventListener("click", classToggler({element: divLoginContent, toggleClass: "show"}));
+btnLoginClose.addEventListener("click", classToggler({element: divLoginContent, toggleClass: "show"}));
+divLoginContent.addEventListener("toggling", classToggler({element: overlay, toggleClass: "show"}));
 
-btnMenuRegister.addEventListener("click", toggleClass(divRegisterContent, "show"));
-btnRegisterClose.addEventListener("click", toggleClass(divRegisterContent, "show"));
-divRegisterContent.addEventListener("toggling", toggleClass(overlay, "show"));
+btnMenuRegister.addEventListener("click", classToggler({element: divRegisterContent, toggleClass: "show"}));
+btnRegisterClose.addEventListener("click", classToggler({element: divRegisterContent, toggleClass: "show"}));
+divRegisterContent.addEventListener("toggling", classToggler({element: overlay, toogleClass: "show"}));
 
 /************** Imagem Cadastro-Viagem ************/
 function PreviewImage() {
