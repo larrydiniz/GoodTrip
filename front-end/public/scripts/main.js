fdd15afe0925775489d88c9bfcfc4621bf42478b
mouraCorazim <lucasmouracorazim1@gmail.com>
2020-09-30 19:30:18 -0300

    modais: função de montagem

--- front-end/public/scripts/main.js
+++ front-end/public/scripts/main.js
@@ -1,14 +1,36 @@
-/************** Menu Overlay ************/
 const nav = document.getElementById("menu");
 const btn = document.querySelector(".menu-landing");
+const overlay = document.querySelector("div.overlay");
+const ulNav = document.querySelector("ul");
 const btnMenuLogin = document.querySelector("button#entrar.menu");
 const btnMenuRegister = document.querySelector("button#cadastrar.menu");
-const divLoginContent = document.querySelector("div.login.modal");
-const divRegisterContent = document.querySelector("div.cadastro.modal");
-const btnLoginClose = document.querySelector("button.login.fechar");
-const btnRegisterClose = document.querySelector("button.cadastro.fechar");
-const ulNav = document.querySelector("ul");
-const overlay = document.querySelector("div.overlay");
+
+
+/****** MODAIS *******/
+
+const loginModal = {
+	openButton: btnMenuLogin,
+	closeButton: document.querySelector("button.login.fechar"),
+	content: document.querySelector("div.login.modal"),
+	modalOverlay: overlay,
+	visibilityClass: "show"
+}
+
+const registerModal = {
+	openButton: btnMenuRegister,
+	closeButton: document.querySelector("button.cadastro.fechar"),
+	content: document.querySelector("div.cadastro.modal"),
+	modalOverlay: overlay,
+	visibilityClass: "show"
+}
+
+function modalMountListeners({openButton, closeButton, content, modalOverlay, visibilityClass}){
+
+	openButton.addEventListener("click", classToggler({element: content, toggleClass: visibilityClass}));
+	closeButton.addEventListener("click", classToggler({element: content, toggleClass: visibilityClass}));
+	content.addEventListener("toggling", classToggler({element: modalOverlay, toggleClass: visibilityClass}));
+}
+
 
 function classToggler({element, toggleClass}){
 	const toggling = new Event('toggling');
@@ -27,18 +49,6 @@ function classToggler({element, toggleClass}){
 	}
 }
 
-btn.addEventListener("click", classToggler({element: nav, toggleClass: "show"}));
-nav.addEventListener("click", classToggler({element: nav, toggleClass: "show"}));
-overlay.addEventListener("toggling", classToggler({element: ulNav, toggleClass: "hide"}));
-
-btnMenuLogin.addEventListener("click", classToggler({element: divLoginContent, toggleClass: "show"}));
-btnLoginClose.addEventListener("click", classToggler({element: divLoginContent, toggleClass: "show"}));
-divLoginContent.addEventListener("toggling", classToggler({element: overlay, toggleClass: "show"}));
-
-btnMenuRegister.addEventListener("click", classToggler({element: divRegisterContent, toggleClass: "show"}));
-btnRegisterClose.addEventListener("click", classToggler({element: divRegisterContent, toggleClass: "show"}));
-divRegisterContent.addEventListener("toggling", classToggler({element: overlay, toggleClass: "show"}));
-
 /************** Imagem Cadastro-Viagem ************/
 function PreviewImage() {
 	var imgReader = new FileReader();
@@ -50,3 +60,11 @@ function PreviewImage() {
 };
 
 
+/***** MAIN *****/
+
+btn.addEventListener("click", classToggler({element: nav, toggleClass: "show"}));
+nav.addEventListener("click", classToggler({element: nav, toggleClass: "show"}));
+overlay.addEventListener("toggling", classToggler({element: ulNav, toggleClass: "hide"}));
+
+modalMountListeners(loginModal);
+modalMountListeners(registerModal);
