34365b9617b611f3cef2826a30c01dcf1316ef6e
Larissa Diniz <larissa.diniz_23@hotmail.com>
2020-10-04 20:05:39 -0300

    tela de adição de tarefa

--- front-end/public/scripts/main.js
+++ front-end/public/scripts/main.js
@@ -94,21 +94,3 @@ function visualizar(id, cod){
 		olho.src = "../public/icons/olho-fechado.svg"
 	}	
 }
-
-
-
-/* 
-const nav = document.getElementById("nav");
-const btn = document.getElementById("dh_menu_btn");
-
-function menu (){
-    let show = nav.classList.contains("show");
-
-        if(show == false){
-            nav.classList.add("show");
-        }else{
-            nav.classList.remove("show");
-        }
-}
-
-btn.addEventListener("click", menu)  */
