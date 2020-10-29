a80d5cf8885688f2fadd2dcfb52c4af925a71fae
mouraCorazim <lucasmouracorazim1@gmail.com>
2020-10-29 07:22:28 -0300

    Index visualização de senhas

--- front-end/public/scripts/index.js
+++ front-end/public/scripts/index.js
@@ -1,6 +1,7 @@
 import menu from "./modules/menu.js"
 import modals from "./modules/modals.js"
-import { classToggler } from "./utils/togglers.js"
+import passwordVisibility from "./modules/passwordVisibility.js"
+import { classToggler, sourceToggler, typeToggler } from "./utils/togglers.js"
 
 const nav = document.getElementById("menu");
 const btn = document.querySelector(".menu-landing");
@@ -12,9 +13,35 @@ const loginModalContent = document.querySelector("div.login.modal");
 const registerModalOpenButton = document.querySelector("button#cadastrar.menu");
 const registerModalCloseButton = document.querySelector("button.cadastro.fechar");
 const registerModalContent = document.querySelector("div.cadastro.modal");
+const passwordRegisterButton = document.querySelector("div#botao-cadastro-senha");
+const passwordRegisterInput = document.querySelector("input#cadastro-senha");
+const passwordConfirmButton = document.querySelector("div#botao-confirma-senha");
+const passwordConfirmInput = document.querySelector("input#confirma-senha");
+const passwordLoginButton = document.querySelector("div#botao-senha-login");
+const passwordLoginInput = document.querySelector("input#senha-login");
+
+const passwordInputTypes = Object.defineProperties({}, { primary: { value: "password", writable: false} , secondary: { value: "text", writable: false }});
+const passwordIconSources = Object.defineProperties({}, { primary: { value: "../public/icons/olho-fechado.svg", writable: false} , secondary: { value: "../public/icons/olho-aberto.svg", writable: false }});
 
 const md = modals(classToggler);
 const mnu = menu(classToggler);
+const pv = passwordVisibility(typeToggler, sourceToggler);
+
+const passwordRegisterField = pv.defineField({ button: passwordRegisterButton, 
+									           input: passwordRegisterInput, 
+									           types: passwordInputTypes,
+									           sources: passwordIconSources});
+
+
+const confirmPasswordField = pv.defineField({ button: passwordConfirmButton,
+		                                      input: passwordConfirmInput,
+		                                      types: passwordInputTypes,
+											  sources: passwordIconSources});
+
+const loginPasswordField = pv.defineField({ button: passwordLoginButton,
+											input: passwordLoginInput,
+											types: passwordInputTypes,
+											sources: passwordIconSources});
 
 const menuMobile = mnu.defineMenu({ openButton: btn,
 								    content: nav, 
@@ -44,3 +71,12 @@ md.addTogglingListeners({ modal: loginModal });
 md.addOpenedListeners({ modal: registerModal });
 md.addClosedListeners({ modal: registerModal });
 md.addTogglingListeners({ modal: registerModal });
+
+pv.addTypeListeners({ field: passwordRegisterField });
+pv.addSourceListeners({ field: passwordRegisterField });
+
+pv.addTypeListeners({ field: confirmPasswordField });
+pv.addSourceListeners({ field: confirmPasswordField });
+
+pv.addTypeListeners({ field: loginPasswordField });
+pv.addSourceListeners({ field: loginPasswordField });
