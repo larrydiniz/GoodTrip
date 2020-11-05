491593fda1407d48185b527d59670e60eea8dd12
mouraCorazim <lucasmouracorazim1@gmail.com>
2020-11-05 14:35:29 -0300

    Servidor configurado

--- back-end/goodtrip/src/main/java/br/com/pi/goodtrip/controllers/EmbarqueController.java
+++ back-end/goodtrip/src/main/java/br/com/pi/goodtrip/controllers/EmbarqueController.java
@@ -1,10 +1,12 @@
 package br.com.pi.goodtrip.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("embarques")
 public class EmbarqueController {
 	
