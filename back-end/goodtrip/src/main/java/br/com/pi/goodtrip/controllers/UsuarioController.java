5a0cd18783ef0c1f0061e104a1a12ce5f3416a99
Larissa Diniz <larissa.diniz_23@hotmail.com>
2020-11-10 00:14:42 -0300

    busca por usuário

--- back-end/goodtrip/src/main/java/br/com/pi/goodtrip/controllers/UsuarioController.java
+++ back-end/goodtrip/src/main/java/br/com/pi/goodtrip/controllers/UsuarioController.java
@@ -1,5 +1,6 @@
 package br.com.pi.goodtrip.controllers;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.pi.goodtrip.models.Usuario;
@@ -26,6 +28,11 @@ public class UsuarioController {
 	@GetMapping("ler/{id}")
 	public Optional<Usuario> lerConvite(@PathVariable(value = "id") int id){
 		return repository.findById(id);
+	}
+	
+	@GetMapping("/buscar")
+	public List<Object[]> getByNome(@RequestParam String q) {
+		return repository.encontrarUsuario(q);
 	}
 	
 	@PostMapping("escrever")
