943ebcfa2cab9726537afd4fed08c6cbbf9bd273
mouraCorazim <lucasmouracorazim1@gmail.com>
2020-11-10 04:43:20 -0300

    Requisição de criação de tarefa

--- front-end/public/scripts/novaTarefa.js
+++ front-end/public/scripts/novaTarefa.js
@@ -1,16 +1,42 @@
 import urlParser from './modules/urlParser.js';
 import taskCards from "./modules/taskCards.js"
 
+const urlp = urlParser();
+
 const dia = document.querySelector('div#dia');
 const tarefas = document.querySelector('div.tarefas');
 const templateCardTarefas = document.getElementById('t-card-tarefa');
-
-const urlp = urlParser();
-
+const descriptionInput = document.querySelector('textarea#descricao');
+const titleInput = document.querySelector('input#titulo');
+const costInput = document.querySelector('input#custo');
+const hourInput = document.querySelector('input#hora');
+const minInput = document.querySelector('input#min');
+const sendButton = document.querySelector('button.btn-salvar');
+const radioInputs = [...document.getElementsByName('moeda'), ...document.getElementsByName('transporte')];
+const inputsList = [minInput, hourInput, titleInput, costInput, descriptionInput];
 const urlParams = urlp.mapVariables(location.href);
 const diaSelecionado = urlParams.day.split("-")[2];
 
 dia.innerHTML = `${diaSelecionado}`;
+
+function addZero(value){
+	return value.length < 2? ("0" + value): value;
+}
+
+sendButton.addEventListener('click', () => {
+
+	const inputsValues = radioInputs.filter(radio => radio.checked)
+									.concat(...inputsList)
+									.reduceRight((acc, currentInput, index, array) => index + 5 === array.length? (acc[currentInput.name] += ":" + addZero(currentInput.value), acc): (acc[currentInput.name] = currentInput.value, acc), {})
+
+	const paramsValues = { "data": urlParams.day,
+						   "viagem": { "id": Number.parseInt(urlParams.travel_id) },
+	                       "usuario": { "id": 1 }}
+
+	console.log(inputsValues, paramsValues);
+
+	fetch(`http://localhost:3333/tarefas/escrever`, { headers: { "Content-Type": "application/json" }, method: "POST", body: JSON.stringify({...inputsValues, ...paramsValues}), redirect: "follow"})
+})
 
 fetch("/data/tarefas.json")
 	.then(res => res.json())
