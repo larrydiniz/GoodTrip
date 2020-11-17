ef4e04aa81de829ada2c3b00bb42809d3c457a90
mouraCorazim <lucasmouracorazim1@gmail.com>
2020-11-17 06:55:32 -0300

    Upload de imagem; caminhos relativos; imagem default; requisições

--- front-end/public/scripts/edicaoDePerfil.js
+++ front-end/public/scripts/edicaoDePerfil.js
@@ -1,13 +1,12 @@
 import updateUserInfos from "./requests/updateUserInfos.js"
 import imagePreviewer from "./utils/imagePreviewer.js"
 
-const inputDescription = document.querySelector('textarea#edicao_descricao');
 const inputUsername = document.querySelector('input#edicao_nome_usuario');
 const inputName = document.querySelector('input#edicao_nome_perfil');
 const inputImg = document.querySelector('input#edicao_perfil_inputImagem');
 const imgPreview = document.querySelector('img#edicao_perfil_img');
 const sendButton = document.querySelector('button#edicao_salvar');
-const inputsList = [inputName, inputUsername, inputImg, inputDescription];
+const inputsList = [inputName, inputUsername];
 									
 inputImg.addEventListener('change', imagePreviewer({input: inputImg, previewBox: imgPreview}));
 
@@ -20,4 +19,17 @@ sendButton.addEventListener('click', () => {
     fetch(request.url, request.init)
         .then(res => res.json())
         .then(res => console.log(res))
+})
+
+sendButton.addEventListener('click', () => {
+
+    const formdata = new FormData();
+
+    formdata.append("foto", inputImg.files[0]);
+
+    const requestBody = formdata;
+
+    const request = postUploadTravelImage(requestBody)
+
+    fetch(request.url, request.init)
 })
